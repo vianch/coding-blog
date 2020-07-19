af8e0e2b1dfc230ed8c8b2c772bc51a1efe181a9
Victor <vachavarro@gmail.com>
2020-07-18 23:22:32 -0500

    feat(folder_structure): add favicon

--- frontend/website/pages/_document.js
+++ frontend/website/pages/_document.js
@@ -46,6 +46,27 @@ class WebDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
             rel="stylesheet"
           />
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <link
+            href="/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
+          <link
+            href="/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link href="/site.webmanifest" rel="manifest" />
+          <link color="#5bbad5" href="/safari-pinned-tab.svg" rel="mask-icon" />
+          <meta content="#ffffff" name="theme-color" />
         </Head>
         <body>
           <Main />
