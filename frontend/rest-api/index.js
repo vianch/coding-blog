e5203cafad6795543e4fd7b4fe707a942ee946d8
Victor Chavarro <vachavarro@gmail.com>
2020-07-19 18:43:08 -0500

    rest api logic (#2)
    * feat(rest_api_environmnet): add rest api structure
    
    * feat(rest_api_environment): create rest logic endpoints
    
    * feat(rest_api_environment): create asset server

---
+++ frontend/rest-api/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const helmet = require("helmet");
+const path = require("path");
+
+const { restApiEnvironmentSetup } = require("./core/env");
+const { serverLogger } = require("./core/logger");
+const { defaultPort, apiEnvironments } = require("./core/constants");
+const { dataBaseSetup } = require("./core/mongoose");
+const appRoutes = require("./routes/index.js");
+/* Environment configuration */
+restApiEnvironmentSetup();
+
+/* Database configuration */
+const { NODE_EVN, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
+dataBaseSetup(
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
+  NODE_EVN === apiEnvironments.DEVELOPMENT
+);
+
+const port = process.env.SERVER_PORT || defaultPort;
+const app = express();
+
+app.use(helmet());
+app.use(appRoutes);
+app.use("/assets", express.static(path.join(__dirname, "..", "..", "assets")));
+
+app.listen(port, () => {
+  serverLogger.info(`rest-api listening on port ${port}`);
+});
---
+++ frontend/rest-api/routes/api.js
@@ -0,0 +1,47 @@
+const moment = require("moment");
+
+const BlogPostModel = require("../models/post.js");
+const { responseHandler } = require("./errorHandler");
+
+const properties = "title urlTitle dateTimestamp tags thumbnailImageUrl";
+const now = moment().unix();
+const dateTimestamp = { $lte: now };
+const sortAsc = { dateTimestamp: -1 };
+
+const getAllBlogPosts = (callback) => {
+  BlogPostModel.find({ dateTimestamp }, properties)
+    .sort(sortAsc)
+    .exec((error, posts) => responseHandler(error, posts, callback));
+};
+
+const getBlogPostsByTag = (tag, callback) => {
+  BlogPostModel.find({ tags: tag, dateTimestamp }, properties)
+    .sort(sortAsc)
+    .exec((error, posts) => responseHandler(error, posts, callback));
+};
+
+const getNewestPosts = (limit, callback) => {
+  const max = limit ? parseInt(limit) : 2;
+
+  BlogPostModel.find({ dateTimestamp }, properties)
+      .sort(sortAsc)
+      .limit(max)
+      .exec((error, posts) => responseHandler(error, posts, callback));
+};
+
+const getBlogPostByUrlTitle = (urlTitle, callback) => {
+  BlogPostModel.findOne({urlTitle,}).exec((error, post) => {
+    if(!post) {
+      callback({notFoundError: true});
+    } else {
+      responseHandler(error, post, callback);
+    }
+  });
+};
+
+module.exports = {
+  getAllBlogPosts,
+  getBlogPostsByTag,
+  getNewestPosts,
+  getBlogPostByUrlTitle
+};
